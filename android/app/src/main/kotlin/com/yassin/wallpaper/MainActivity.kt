cd3c152bbdeee28331f060c7030c4df73da12e9a
Yassin522 <yasinalmhdi8@gmail.com>
2022-08-15 15:44:35 +0300

    finally

---
+++ android/app/src/main/kotlin/com/yassin/wallpaper/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yassin.wallpaper
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/config.dart
@@ -0,0 +1,26 @@
+
+
+class Config {
+  
+  final String hashTag = '#wallme';
+  final String appName = 'Wallme';
+  
+  final String packageName = 'mrblab.wallpaper_app';
+  final String appVersion = '3.0.1';
+
+  final String appIcon = 'assets/images/icon.png';
+  final String splashIcon = 'assets/images/splash.png';
+  final String guestAvatar = 'assets/images/guest_avatar.png';
+
+  // -- Onesignal Push Notification
+  final String onesignalAppId = 'b07c0a8d-dec6-4342-8f72-433dace8453f';
+  
+  //-- admob ads --
+  final String admobAppId = 'ca-app-pub-3940256099942544~3347511713';
+  final String admobInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+
+
+  //-- facebook ads--
+  final String facebookInterstitialAdId = '5878408****************';
+
+}
